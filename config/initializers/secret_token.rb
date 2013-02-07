ba4eb3e60995da1efc03de86b931e4094b19596c
Thaironius <thaironius@gmail.com>
2013-02-06 22:01:00 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyFi::Application.config.secret_token = '4939790d49194fbfa473ab37df0a2810aae0887d3ba1f37276d059d10dd36a94c68bbd77bf47332f01b9ed972b2bf430b51834eb9637202ce9aea67245a8778f'
